49c1a9afc3cfa1f8af2c156831ca86d0af1e6850
Atul Thakre <atulthakre00@gmail.com>
2020-03-15 09:57:14 +0530

    Modules imported in the file

--- machineInstructions.py
+++ machineInstructions.py
@@ -1,3 +1,9 @@
+import display
+import machineInstructions
+import relocator
+import tableGenerator
+import valueCalculator
+
 def machInstn(relLocn , symTab , bTab , litTab , pseudoType , rrType , rxType):
     (n,i) = (len(relLocn),0)
     basePointer = -1
@@ -11,18 +17,18 @@ def machInstn(relLocn , symTab , bTab , litTab , pseudoType , rrType , rxType):
             temp = relLocn[i][1][2].split(',')
             if len(temp) == 2:
                 if not(temp[0].isnumeric()):
-                    t = valFromSymbol(temp[0],symTab)
+                    t = valueCalculator.valFromSymbol(temp[0],symTab)
                     if t == -1:
-                        t = valFromLiteral(temp[0],litTab)
+                        t = valueCalculator.valFromLiteral(temp[0],litTab)
                     temp[0] = t
                 if not(temp[1].isnumeric()):
-                    t = valFromSymbol(temp[1],symTab)
+                    t = valueCalculator.valFromSymbol(temp[1],symTab)
                     if t == -1:
-                        t = valFromLiteral(temp[1],litTab)
+                        t = valueCalculator.valFromLiteral(temp[1],litTab)
                     temp[1] = t
             else:
                 if not(temp[0].isnumeric()):
-                    temp[0] = valFromSymbol(temp[0],symTab)
+                    temp[0] = valueCalculator.valFromSymbol(temp[0],symTab)
                 if relLocn[i][1][1] == "BR":
                     temp.insert(0,15)
                     cmd = "BCR"                
@@ -38,18 +44,18 @@ def machInstn(relLocn , symTab , bTab , litTab , pseudoType , rrType , rxType):
             if len(temp) == 2:
                 index = 0
                 if not(temp[0].isnumeric()):
-                    temp[0] = valFromSymbol(temp[0],symTab)
+                    temp[0] = valueCalculator.valFromSymbol(temp[0],symTab)
                 if temp[1].find(')') != -1 and temp[1][0] != '=':
-                    index = indexExtractor(temp[1],bTab,symTab)
+                    index = valueCalculator.indexExtractor(temp[1],bTab,symTab)
                     if temp[1][0] != '=':
                         temp[1] = temp[1][:temp[1].find('(')]
                 if not(temp[1].isnumeric()):
-                    t = valFromSymbol(temp[1],symTab)
+                    t = valueCalculator.valFromSymbol(temp[1],symTab)
                     if t == -1:
-                        t = valFromLiteral(temp[1],litTab)  
+                        t = valueCalculator.valFromLiteral(temp[1],litTab)  
                     temp[1] = t
                 
-                temp[1] = calcAddress(temp[1],index,basePointer,bTab)
+                temp[1] = valueCalculator.calcAddress(temp[1],index,basePointer,bTab)
                 arg = str(temp[0]) + "," + str(temp[1])                
                 machTab.append([relLocn[i][0],[cmd , arg]])  
         elif relLocn[i][1][1] == 'LTORG' or relLocn[i][1][1] == 'DS' or relLocn[i][1][1] == 'DC':
